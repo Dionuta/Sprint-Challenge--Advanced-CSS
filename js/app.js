e73cf94da9e6ae35869a340afbcfbd57c2792fdf
Dionuta <diounta@gmail.com>
2019-01-12 18:14:10 -0800

    toggle button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+// Sign Up pop up.
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+  }
+
+// toggle button 
+  function openForm() {
+    var x = document.getElementById("myForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
